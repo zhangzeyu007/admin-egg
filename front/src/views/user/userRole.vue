e3f8ffef32f2e358dfcf7336fc9b2c06b7638e42
zhangzeyu007 <zhangzeyu1234@gmail.com>
2021-03-24 09:53:30 +0800

    zzy

--- front/src/views/user/userRole.vue
+++ front/src/views/user/userRole.vue
@@ -3,25 +3,20 @@
  * @Author: 海象
  * @Date: 2021-02-06 10:18:40
  * @LastEditors: 海象
- * @LastEditTime: 2021-02-06 10:18:56
+ * @LastEditTime: 2021-03-24 09:48:33
 -->
 
 <template>
-  <div class="userRole">
-      用户权限
-  </div>
+  <div class="userRole"></div>
 </template>
 
 <script>
 export default {
-   data() {
-       return {
-       };
-   },
-   mounted() {
-   },
-   methods: {
-   }
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
 };
 </script>
 
