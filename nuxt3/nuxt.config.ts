94558ae97a462de76353c8bdc89f9779674d372f
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-04-07 16:09:14 +0800

    feat:ssr

--- nuxt3/nuxt.config.ts
+++ nuxt3/nuxt.config.ts
@@ -1,4 +1,14 @@
+/*
+ * @Description: 
+ * @Author: 张泽雨
+ * @Date: 2024-04-06 15:55:22
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2024-04-07 15:27:35
+ * @FilePath: \admin-egg\nuxt3\nuxt.config.ts
+ */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: true
+
 })
--- nuxt3/pages/index.vue
+++ nuxt3/pages/index.vue
@@ -2,7 +2,16 @@
   <div>
     首页
     <img class="logo" src="../assets/image/logo.png" alt="" />
-
+    <h1>你还哦啊</h1>
+    <div>你还哦啊你还哦啊</div>
+    <div>你还哦啊你还哦啊</div>
+    <div>你还哦啊你还哦啊</div>
+    <div>你还哦啊你还哦啊</div>
+    <div>你还哦啊你还哦啊</div>
+    <div>你还哦啊你还哦啊</div>
+    <div>你还哦啊你还哦啊</div>
+    <div>你还哦啊你还哦啊</div>
+    <div>你还哦啊你还哦啊</div>
     <div v-for="item in res" :key="item.id">{{ item.title }}</div>
   </div>
 </template>
@@ -13,7 +22,6 @@ const { data } = await useFetch<{ data: any }>(
   { method: "POST", body: { pageNum: 1, total: 10, pageSize: 10 } }
 );
 const res = data.value?.data.page;
-console.log(res);
 </script>
 
 <style>
